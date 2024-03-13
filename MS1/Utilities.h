4b33e9a34d4d09073241ab2269d30f8a52dd2f90
Vinicius Hayden <67245806+vinicius-hayden@users.noreply.github.com>
2024-03-13 19:01:13 +0000

    utilities class created

---
+++ MS1/Utilities.h
@@ -0,0 +1,29 @@
+// Name: Vinicius Souza da Silva
+// Seneca Student ID: 135067221
+// Seneca email: vhsouza-da-silva@myseneca.ca
+// Date of completion: 13/03/2024
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SENECA_UTILITIES_H
+#define SENECA_UTILITIES_H
+
+#include <string>
+
+namespace seneca {
+    class Utilities {
+            size_t m_widthField{};
+            static char m_delimiter{};
+        public:
+            void setFieldWidth(size_t newWidth);
+            size_t getFieldWidth() const;
+            std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+            
+            static void setDelimiter(char newDelimiter);
+            static char getDelimiter() const
+        ;
+    }
+}
+
+#endif // SENECA_UTILITIES_H
