970d52b731181a17e2e3234bc0d57791868dbe49
Vinicius Hayden <67245806+vinicius-hayden@users.noreply.github.com>
2024-03-13 19:09:13 +0000

    default constructor

--- MS1/Utilities.h
+++ MS1/Utilities.h
@@ -16,6 +16,7 @@ namespace seneca {
             size_t m_widthField{};
             static char m_delimiter{};
         public:
+            Utilities() = default;
             void setFieldWidth(size_t newWidth);
             size_t getFieldWidth() const;
             std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
